6e24ec848c3043db68f9e91adc20dacd8e91d173
nguyen huu tri <huutriqt13@hotmail.com>
2023-05-22 19:41:38 +0700

    add result screen

---
+++ lib/screen/summary_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SummaryItem extends StatelessWidget {
+  final Map<String, Object> itemData;
+
+  const SummaryItem(this.itemData, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Gen other widget
+
+          const SizedBox(
+            height: 5,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  itemData['question'] as String,
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  itemData['user_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 202, 171, 252),
+                  ),
+                ),
+                Text(
+                  itemData['correct_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 96, 176, 166),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
